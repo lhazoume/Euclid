079e6ab37ced55af33c554b4a4c54196e05b1165
lhazoume <luc@hazoume.net>
2016-09-15 21:19:47 +0200

    IDensityKernel
    Minor comments

--- Euclid/Distributions/Continuous/Kernels/IDensityKernel.cs
+++ Euclid/Distributions/Continuous/Kernels/IDensityKernel.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Euclid.Distributions.Continuous.Kernels
+﻿namespace Euclid.Distributions.Continuous.Kernels
 {
+    /// <summary>
+    /// Interface for all density kernels
+    /// </summary>
     public interface IDensityKernel
     {
+        /// <summary>
+        /// the kernel function
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>a double</returns>
         double K(double x);
+        /// <summary>
+        /// the left hand side integral of the kernel function
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>a double</returns>
         double IntegralK(double x);
+
+        /// <summary>
+        /// Returns the integral of t^2*K(t)
+        /// </summary>
         double Variance { get; }
     }
 }
