6d2376629bed03989f113517b2ebc5faf8af92ca
lhazoume <luc@hazoume.net>
2021-12-19 19:32:37 +0100

    Statistics summary Comments added on Diffentiator Minor optimisation on Bracketing

--- Euclid/Analytics/Statistics/SummaryStatistics.cs
+++ Euclid/Analytics/Statistics/SummaryStatistics.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Euclid.Analytics.Statistics
+﻿namespace Euclid.Analytics.Statistics
 {
     /// <summary>
     /// Class which encapsulate a glossary of statistics
@@ -12,53 +6,39 @@ namespace Euclid.Analytics.Statistics
     public sealed class SummaryStatistics
     {
         #region vars
-        /// <summary>
-        /// Minimum
-        /// </summary>
+        /// <summary>Returns the minimum</summary>
         public double Min { get; private set; }
-        /// <summary>
-        /// Average
-        /// </summary>
+
+        /// <summary>Returns the average</summary>
         public double Average { get; private set; }
-        /// <summary>
-        /// Maximum
-        /// </summary>
+
+        /// <summary>Returns the maximum</summary>
         public double Max { get; private set; }
-        /// <summary>
-        /// Standard deviation (Pearson)
-        /// </summary>
+        /// <summary>Standard deviation (Pearson)</summary>
         public double Stdev { get; private set; }
-        /// <summary>
-        /// Distance between Min & Max
-        /// </summary>
+
+        /// <summary>Distance between Min && Max</summary>
         public double Range { get; private set; }
-        /// <summary>
-        /// Median
-        /// </summary>
+
+        /// <summary>Median</summary>
         public double Median { get; private set; }
-        /// <summary>
-        /// Percentile 10%
-        /// </summary>
+
+        /// <summary>Percentile 10%</summary>
         public double Percentile10 { get; private set; }
-        /// <summary>
-        /// Percentile 90%
-        /// </summary>
+
+        /// <summary>Percentile 90%</summary>
         public double Percentile90 { get; private set; }
-        /// <summary>
-        /// Measure of asymmetry
-        /// </summary>
+
+        /// <summary>Measure of asymmetry</summary>
         public double Skewness { get; private set; }
-        /// <summary>
-        /// Measure of the tailedness
-        /// </summary>
+
+        /// <summary>Measure of the tailedness</summary>
         public double Kurtosis { get; private set; }
-        /// <summary>
-        /// Sum of element(s)
-        /// </summary>
+
+        /// <summary>Sum of element(s)</summary>
         public double Sum { get; private set; }
-        /// <summary>
-        /// Number of element(s)
-        /// </summary>
+
+        /// <summary>Number of element(s)</summary>
         public int Count { get; private set; }
         #endregion
 
@@ -67,9 +47,8 @@ namespace Euclid.Analytics.Statistics
         /// Default constructor
         /// </summary>
         public SummaryStatistics() { }
-        /// <summary>
-        /// Constructor of SummaryStatistics
-        /// </summary>
+
+        /// <summary>Builds a <c>SummaryStatistics</c></summary>
         /// <param name="min">Minimum</param>
         /// <param name="average">Average</param>
         /// <param name="max">Maximum</param>
--- Euclid/Numerics/Differentiator.cs
+++ Euclid/Numerics/Differentiator.cs
@@ -5,9 +5,7 @@ using System.Linq;
 
 namespace Euclid.Numerics
 {
-    /// <summary>
-    /// Numeric differentiation class
-    /// </summary>
+    /// <summary>Numeric differentiation class</summary>
     public static class Differentiator
     {
         /// <summary> First degree numerical differentiation</summary>
@@ -18,9 +16,7 @@ namespace Euclid.Numerics
             return Differentiate(function, DifferenceForm.Central, Descents.STEP_EPSILON);
         }
 
-        /// <summary>
-        /// First degree numerical differentiation
-        /// </summary>
+        /// <summary>First degree numerical differentiation</summary>
         /// <param name="function">the function to differentiate</param>
         /// <param name="form">the differentiation form</param>
         /// <param name="step">the differentiation step</param>
@@ -41,7 +37,7 @@ namespace Euclid.Numerics
         /// <param name="function">the function to differentiate</param>
         /// <param name="n">the degree</param>
         /// <param name="form">the differentiation form</param>
-        /// <param name="step">the differentiation degree</param>
+        /// <param name="step">the differentiation step</param>
         /// <returns>the n-degree derivative</returns>
         public static Func<double, double> Differentiate(this Func<double, double> function, int n, DifferenceForm form, double step)
         {
@@ -61,29 +57,36 @@ namespace Euclid.Numerics
             }
         }
 
-        public static Matrix Hessian(Func<Vector, double> func, Vector center, Vector bump)
+        /// <summary> High degree numerical differentiation</summary>
+        /// <param name="function">the function to differentiate</param>
+        /// <param name="bump">the differentiation step</param>
+        /// <returns>the hessian matrix</returns>
+        public static Func<Vector, Matrix> Hessian(this Func<Vector, double> function, Vector bump)
         {
-            if (bump.Size != center.Size || bump.Data.Any(d => d <= 0))
+            if (bump.Data.Any(d => d <= 0))
                 throw new ArgumentException("bump sign irrelevant", nameof(bump));
-            int n = center.Size;
-            Matrix result = Matrix.Create(n, n);
-            double refValue = func(center);
-
-            for (int i = 0; i < n; i++)
+            
+            return x =>
             {
-                Vector iBump = Vector.ExtractIthDimension(bump, i);
-                result[i, i] = (func(center + iBump) + func(center - iBump) - 2 * refValue) / Math.Pow(bump[i], 2);
+                int n = bump.Size;
+                Matrix result = Matrix.Create(n, n);
+                double refValue = function(x);
 
-                for (int j = i + 1; j < n; j++)
+                for (int i = 0; i < n; i++)
                 {
-                    Vector jBump = Vector.ExtractIthDimension(bump, j);
-                    result[i, j] = (func(center + iBump + jBump) + func((center - iBump) - jBump)
-                        - func((center + iBump) - jBump) - func((center + jBump) - iBump)) / (4 * bump[i] * bump[j]);
-                    result[j, i] = result[i, j];
-                }
-            }
-            return result;
+                    Vector iBump = Vector.ExtractIthDimension(bump, i);
+                    result[i, i] = (function(x + iBump) + function(x - iBump) - 2 * refValue) / Math.Pow(bump[i], 2);
 
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        Vector jBump = Vector.ExtractIthDimension(bump, j);
+                        result[i, j] = (function(x + iBump + jBump) + function((x - iBump) - jBump)
+                            - function((x + iBump) - jBump) - function((x + jBump) - iBump)) / (4 * bump[i] * bump[j]);
+                        result[j, i] = result[i, j];
+                    }
+                }
+                return result;
+            };
         }
     }
 }
