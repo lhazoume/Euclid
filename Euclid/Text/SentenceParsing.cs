3cfbec296779f169708efd1455bbf80ad4fbace5
lhazoume <luc@hazoume.net>
2022-08-14 20:04:36 +0200

    Update SentenceParsing.cs

--- Euclid/Text/SentenceParsing.cs
+++ Euclid/Text/SentenceParsing.cs
@@ -3,8 +3,14 @@ using System.Linq;
 
 namespace Euclid.Text
 {
+    /// <summary>Helps parsing sentences</summary>
     public static class SentenceParsing
     {
+        /// <summary>Counts the words that are common to two sentences</summary>
+        /// <param name="sentenceA">the first sentence</param>
+        /// <param name="sentenceB">the second sentence</param>
+        /// <returns>an int</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static int CommonWords(string sentenceA, string sentenceB)
         {
             if (sentenceA == null) throw new ArgumentNullException(nameof(sentenceA));
