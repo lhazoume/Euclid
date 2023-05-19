6ac8eaf58cf59a56d754062295523e6475ac03fa
lhazoume <luc@hazoume.net>
2023-05-19 15:51:35 +0200

    Up version Comments in PiecewiseFunction

--- Euclid/Numerics/PiecewiseFunction.cs
+++ Euclid/Numerics/PiecewiseFunction.cs
@@ -2,43 +2,45 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Euclid.Numerics
 {
+    /// <summary>Represents a piecewise function</summary>
     public class PiecewiseFunction
     {
-        private Interval[] _intervals;
-        private double[] _values;
+        private readonly Interval[] _intervals;
+        private readonly double[] _values;
 
-        public PiecewiseFunction(double lowerPoint, double upperPoint, IList<double> cornerPoints, IList<double> values)
+        /// <summary>Builds a piecewise function</summary>
+        /// <param name="cornerPoints">the corner points</param>
+        /// <param name="values">the bucket values</param>
+        public PiecewiseFunction(IList<double> cornerPoints, IList<double> values)
         {
-            if (values.Count != cornerPoints.Count + 1) throw new ArgumentException("the values' size should exactly the corner points' size + 1");
+            if (values.Count + 1 != cornerPoints.Count) throw new ArgumentException("the values' size should exactly the corner points' size + 1");
 
-            _intervals = new Interval[cornerPoints.Count + 1];
-            _values = new double[cornerPoints.Count + 1];
-            for (int i = 0; i < cornerPoints.Count; i++)
+            _intervals = new Interval[values.Count];
+            _values = new double[values.Count];
+
+            for (int i = 0; i < values.Count; i++)
             {
-                if (i == 0)
-                {
-                    _intervals[0] = new Interval(lowerPoint, cornerPoints[0], true, true);
-                    _values[0] = values[0];
-                }
-                else
-                {
-                    _intervals[i] = new Interval(cornerPoints[i - 1], cornerPoints[i], false, true);
-                    _values[i] = values[i];
-                }
+                _intervals[0] = new Interval(cornerPoints[i], cornerPoints[i + 1], i == 0, true);
+                _values[i] = values[i];
             }
-            _intervals[cornerPoints.Count] = new Interval(cornerPoints.Last(), upperPoint, false, true);
-            _values[cornerPoints.Count] = values.Last();
         }
 
+        /// <summary>Accesses the piecewise function for a given value</summary>
+        /// <param name="t">the input abscissa</param>
+        /// <returns>a double</returns>
         public double this[double t]
         {
             get
             {
+                if (t < _intervals[0].LowerBound.Value || t > _intervals.Last().UpperBound.Value)
+                    throw new ArgumentOutOfRangeException(nameof(t));
+
                 int i = 0;
                 while (i < _intervals.Length && !_intervals[i].Contains(t))
                     i++;
