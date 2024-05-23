369115ef7c77c62998aa8c219ef170cd8c9d6f9d
lhazoume <luc@hazoume.net>
2024-05-23 18:51:58 +0200

    Adding of the benchmarking in Euclid

---
+++ Euclid/Benchmarking/CaseSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euclid.Benchmarking
+{
+    public class CaseSet
+    {
+        private readonly List<Case> _cases;
+
+        public CaseSet(IEnumerable<Case> cases)
+        {
+            _cases = cases.ToList();
+        }
+
+        public List<CaseResult> Run()
+        {
+            List<CaseResult> results = new List<CaseResult>();
+
+            for (int i = 0; i < _cases.Count; i++)
+                results.Add(_cases[i].Run());
+
+            return results;
+        }
+    }
+}
