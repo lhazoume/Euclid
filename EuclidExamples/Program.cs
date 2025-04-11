5ae0cdf475935f336a3b83af6f6995b59b439389
lhazoume <luc@hazoume.net>
2025-04-11 18:40:09 +0200

    Correction du git ignore pour les fichiers de EuclidExamples

---
+++ EuclidExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EuclidExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lancement du projet EuclidExamples...");
+
+            
+            DistributionExample.Binomial();
+            DistributionExample.Poisson();
+            DistributionExample.Normal();
+            DistributionExample.Exponential();
+            DistributionExample.Uniform();
+            DistributionExample.Cauchy();
+        }
+    }
+}
