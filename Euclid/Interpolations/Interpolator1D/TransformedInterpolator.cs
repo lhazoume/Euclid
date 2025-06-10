4e1a99f15113744be4a6e4220a04539154e7b6b4
OscarHerNA <oscar.hernandez@qobaltpartners.com>
2025-06-10 18:35:00 +0200

    Ajout de TransformedInterpolator + Tests

---
+++ Euclid/Interpolations/Interpolator1D/TransformedInterpolator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Euclid.Histograms;
+
+namespace Euclid.Interpolations.Interpolator1D
+{
+    public class TransformedInterpolator : IInterpolator1D
+    {
+        #region Private fields
+        private readonly IInterpolator1D _interpolator;
+        private readonly Func<double, double, double> _forward;
+        private readonly Func<double, double, double> _backward;
+        #endregion
+
+        #region Constructors
+        public TransformedInterpolator(IInterpolator1D interpolator,Func<double, double, double> forward,Func<double, double, double> backward)
+        {
+            _interpolator = interpolator ?? throw new ArgumentNullException(nameof(interpolator));
+            _forward = forward ?? throw new ArgumentNullException(nameof(forward));
+            _backward = backward ?? throw new ArgumentNullException(nameof(backward));
+        }
+        #endregion
+
+        #region Accessors
+        /// <summary> Gets the natural range of the x values for the interpolation. </summary>
+        public Interval Range => _interpolator.Range;
+        /// <summary> Gets a value indicating whether the interpolator allows extrapolation outside the range.</summary>
+        public bool Extrapolation => _interpolator.Extrapolation;
+        /// <summary> Gets a value indicating whether the interpolation is local (as opposed to global</summary>
+        public bool Local => _interpolator.Local;
+        /// <summary> Checks if the specified x-value is within the interpolator's range.</summary>
+        /// <param name="x">The x-value to check.</param>
+        /// <returns><c>true</c> if the value is in range; otherwise, <c>false</c>.</returns>
+        public bool IsInRange(double x) => _interpolator.IsInRange(x);
+        /// <summary> Returns a clone of the current <see cref="Transformateur"/> instance. </summary>
+        /// <returns>A new <see cref="IInterpolator1D"/> that is a copy of this instance.</returns>
+        public IInterpolator1D Clone() => new TransformedInterpolator(_interpolator.Clone(), _forward, _backward);
+        #endregion
+
+        #region Methods
+        /// <summary> Sets the data for the interpolation by transforming the points using the forward function.</summary>
+        /// <param name="points">The collection of 2D points to be transformed and used for interpolation.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="points"/> is null.</exception>
+        public void SetData(IEnumerable<Point2D> points)
+        {
+            if (points == null) throw new ArgumentNullException(nameof(points));
+
+            List<Point2D> pts = points.Select(p => new Point2D(p.X, _forward(p.X, p.Y))).OrderBy(p => p.X).ToList();
+
+            _interpolator.SetData(pts);
+        }
+        /// <summary>Sets the data for the interpolation using two lists of x and y values. </summary>
+        /// <param name="x">The list of x-values (abscissas).</param>
+        /// <param name="y">The list of y-values (ordinates).</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="x"/> or <paramref name="y"/> is null.</exception>
+        /// <exception cref="Exception">Thrown if <paramref name="x"/> and <paramref name="y"/> do not have the same length.</exception>
+        public void SetData(IList<double> x, IList<double> y)
+        {
+            if (x == null) throw new ArgumentNullException(nameof(x));
+            if (y == null) throw new ArgumentNullException(nameof(y));
+            if (x.Distinct().Count() != x.Count) throw new ArgumentException("duplicate x-values", nameof(x));
+            if (x.Count != y.Count) throw new Exception("the x-values and y-values do not match");
+
+            List<double> ys = new List<double>(y.Count);
+            for (int i = 0; i < y.Count; i++)
+                ys.Add(_forward(x[i], y[i]));
+
+            _interpolator.SetData(x, ys);
+        }
+        /// <summary>Interpolates (or extrapolates) for a given x-value using the backward transformation.</summary>
+        /// <param name="x">The x-value for which to interpolate.</param>
+        /// <returns>The interpolated (or extrapolated) y-value after applying the backward transformation.</returns>
+        public double ValueAt(double x)
+        {
+            double yPrime = _interpolator.ValueAt(x);
+            return _backward(x, yPrime);
+        }
+        #endregion
+
+    }
+}
---
+++ EuclidTests/TransformedInterpolatorTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Euclid.Interpolations.Interpolator1D;
+
+namespace Euclid.Tests
+{
+    [TestClass]
+    public class TransformedInterpolatorTests
+    {
+        private const double Tolerance = 1e-9;
+
+        #region Identity Transform
+        [TestMethod]
+        public void IdentityTransform_MatchesCore()
+        {
+            List<double> x = new List<double> { 0.0, 1.0, 2.0, 3.0 };
+            List<double> y = new List<double> { 0.0, 2.0, 4.0, 6.0 };
+
+            Hyman interpalator = new Hyman(false);
+            interpalator.SetData(x, y);
+            //Identite 
+            TransformedInterpolator trans = new TransformedInterpolator(interpolator: new Hyman(false),forward: (t, v) => v,backward: (t, v) => v);
+
+            trans.SetData(x, y);
+
+            for (int i = 0; i <= 100; i++)
+            {
+                double xi = 3.0 * i / 100.0;
+                double yin = interpalator.ValueAt(xi);
+                double yt = trans.ValueAt(xi);
+                Assert.AreEqual(yin, yt, Tolerance, $"x={xi:F3}: interpolator={yin:F9} vs trans={yt:F9}");
+            }
+        }
+        #endregion
+
+        #region Square‐via‐Transform
+        [TestMethod]
+        public void SquareFunction_InterpolatesViaSqrtSquareTransform()
+        {
+            List<double> x = new List<double> { 0.0, 1.0, 2.0, 3.0, 4.0 };
+            List<double> y = x.Select(xx => xx * xx).ToList();
+
+            LinearInterpolation interpolator = new LinearInterpolation(false);
+            TransformedInterpolator transformed = new TransformedInterpolator(interpolator: interpolator,forward: (t, yy) => Math.Sqrt(yy),backward: (t, zz) => zz * zz);
+
+            transformed.SetData(x, y);
+
+            for (int i = 0; i <= 500; i++)
+            {
+                double xi = 4.0 * i / 500.0;
+                double yf = transformed.ValueAt(xi);
+                double ytrue = xi * xi;
+                Assert.AreEqual(ytrue, yf,Tolerance, $"x={xi:F3}: attendu={ytrue:F6}, obtenu={yf:F6}");
+            }
+        }
+        #endregion
+
+        #region Performance Benchmark
+        [TestMethod]
+        public void PerformanceBenchmark_ValueAt()
+        {
+            List<double> x = new List<double> { 0.0, 1.0, 2.0, 3.0, 4.0 };
+            List<double> y = x.Select(xx => Math.Exp(-0.03 * xx)).ToList();
+
+            TransformedInterpolator transformed = new TransformedInterpolator(interpolator: new Hyman(false),forward: (t, df) => -Math.Log(df),backward: (t, zt) => Math.Exp(-zt));
+            transformed.SetData(x, y);
+            transformed.ValueAt(2.3);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                double t = 4.0 * (i % 5000) / 5000.0; // [0,4] cycle
+                transformed.ValueAt(t);
+            }
+            sw.Stop();
+
+            Assert.IsTrue(sw.ElapsedMilliseconds < 200,$"Trop lent : {sw.ElapsedMilliseconds} ms pour {N} appels");
+        }
+        #endregion
+
+        #region Error
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetData_DuplicateX()
+        {
+            List<double> x = new List<double> { 0.0, 1.0, 1.0, 2.0 };
+            List<double> y = new List<double> { 1.0, 2.0, 3.0, 4.0 };
+
+            TransformedInterpolator transformed = new TransformedInterpolator(interpolator: new Hyman(false),forward: (t, v) => v,backward: (t, v) => v);
+            transformed.SetData(x, y);
+        }
+        #endregion
+    }
+}
